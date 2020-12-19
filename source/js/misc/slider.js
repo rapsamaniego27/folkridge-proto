fc43124bbce7624d05d9520195b8229fec285371
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-12-19 14:30:05 +0800

    added about section

--- source/js/misc/slider.js
+++ source/js/misc/slider.js
@@ -1,1 +1,11 @@
-console.log('Slidning js');
+var mySwiper = new Swiper('.swiper-container', {
+ // Optional parameters
+ loop: true,
+
+ // Navigation arrows
+ navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+ }
+
+});
